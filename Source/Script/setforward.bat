7a4d210b8ad730ef7b67e798eccd0be9510c3098
kravtsov <kravtsov@shtrih-m.,ru>
2017-08-29 12:34:25 +0300

    [-] Джобавлена команда для установки статического IP адреса адаптеру RNDIS

--- Source/Script/setforward.bat
+++ Source/Script/setforward.bat
@@ -1,3 +1,7 @@
+
+WMIC Path Win32_NetworkAdapterConfiguration WHERE "IPEnabled = 'True' AND ServiceName='usb_rndisx'" call enablestatic ("192.168.137.1"), ("255.255.255.0")
+
+
 net start remoteaccess
 netsh interface portproxy reset
 netsh interface portproxy add v4tov4 listenport=7778 connectaddress=192.168.137.111 connectport=7778
